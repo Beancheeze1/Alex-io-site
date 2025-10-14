12a9f40b41086c9bd1a3b5f0030afc536d8c0e72
Beancheeze1 <25thhourdesign@gmail.com>
2025-10-13 20:29:18 -0400

    Update route.js

--- alex-io-site/alex-io-site-standard-full/app/api/_diag/env/route.js
+++ alex-io-site/alex-io-site-standard-full/app/api/_diag/env/route.js
@@ -1,1 +1,8 @@
+export async function GET() {
+  return new Response(JSON.stringify({
+    ok: true,
+    hasToken: Boolean(process.env.HUBSPOT_ACCESS_TOKEN),
+    autoComment: String(process.env.AUTO_COMMENT || "false")
+  }), { headers: { "Content-Type": "application/json" }, status: 200 });
+}
 
