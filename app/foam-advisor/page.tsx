dd089f061376ac2f501bd8005fed0577e87bae5c
Beancheeze1 <25thhourdesign@gmail.com>
2025-11-30 12:52:29 -0500

    foam advisor rev20

--- app/foam-advisor/page.tsx
+++ app/foam-advisor/page.tsx
@@ -905,7 +905,7 @@ export default function FoamAdvisorPage({
       className="absolute inset-y-0"
       style={{
         left: `${pct}%`,
-        transform: "translateX(-50%) translateX(1px)",
+        transform: "translateX(-50%) translateX(3px)",
       }}
     >
 
