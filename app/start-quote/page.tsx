c027e589a568b3e1b2b779c45d355b1ca0353771
Beancheeze1 <25thhourdesign@gmail.com>
2026-02-07 16:13:26 -0500

    fix ts error

--- app/start-quote/page.tsx
+++ app/start-quote/page.tsx
@@ -1021,7 +1021,7 @@ export default function StartQuotePage() {
               />
             </Field>
           </Section>
-          </div>
+          
 
           <div className="mt-5 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
             <button
